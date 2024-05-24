67aeaa1db019b80253cf221508497f5b6c4d6ccd
Mickykore <mikiyasayele6@gmail.com>
2024-05-24 17:50:54 +0300

    Update route parameter name

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -15,6 +15,8 @@ const getAllTransactions = async (req, res) => {
 // Get transaction by ticketID
 const getTransactionByTicketID = async (req, res) => {
     const { tx_ref } = req.params;
+    console.log("ouuuuuuuuuuu",req.params);
+    console.log("okkk", req.params.tx_ref);
     console.log(tx_ref);
     try {
         const transaction = await TicketTransaction.find({ tx_ref });
