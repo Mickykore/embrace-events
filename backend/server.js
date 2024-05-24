0c69109e7d25bcaf3cebf174375e684e3930dcba
Mickykore <mikiyasayele6@gmail.com>
2024-05-24 08:43:36 +0300

    Update CORS origin

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: ['http://localhost:3000'], 
+        origin: ['http://localhost:3000', 'https://embrace-events.onrender.com'], 
         credentials: true
     }
 ));
